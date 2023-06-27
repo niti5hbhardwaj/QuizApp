b43d371ba77f7bacecf76dc5ce17a4dfdc46c6a9
niti5hbhardwaj <73251125+niti5hbhardwaj@users.noreply.github.com>
2023-06-27 14:34:53 +0530

    added welcome_screen animation

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,33 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/chat_api/chat_api.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:quiz_app/screens/quiz_screen.dart';
+import 'chat_api/chat_api.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   static const id = 'welcome_screen';
   const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TextButton(
-          onPressed: () async {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
-            );
-            await getQuestions();
-            if (context.mounted) {
-              Navigator.pushReplacementNamed(context, QuizScreen.id);
-            }
-          },
-          child: const Text("Start Quiz"),
-        ),
+        child: (isLoading)
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SpinKitFoldingCube(
+                    color: Colors.blueGrey.shade900,
+                    size: 100,
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Text(
+                    "Loading Quiz Data",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.blueGrey.shade900,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            : MaterialButton(
+                onPressed: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  await getQuestions();
+                  setState(() {
+                    isLoading = false;
+                  });
+                  if (context.mounted) {
+                    Navigator.pushReplacementNamed(context, QuizScreen.id);
+                  }
+                },
+                color: Colors.blueGrey.shade900,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25)),
+                height: 50,
+                minWidth: 150,
+                elevation: 5,
+                child: const Text(
+                  "Start Quiz",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
       ),
     );
   }
 }
+
+// Future<void> getQuestions() async {
+//   await Future.delayed(Duration(seconds: 2), () {
+//     print("Hello");
+//   });
+// }
