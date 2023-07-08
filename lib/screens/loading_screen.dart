3c33e3086e2597d52f603c57858d3dd9d828893c
niti5hbhardwaj <73251125+niti5hbhardwaj@users.noreply.github.com>
2023-07-08 12:58:25 +0530

    added light mode to loading page

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-Color backgroundColor = Colors.grey.shade900;
-Color foregroundColor = Colors.grey.shade200;
+Color backgroundColor = Colors.grey.shade100;
+Color foregroundColor = Colors.blueGrey.shade900;
 
-class LoadingScreen extends StatefulWidget {
+class LoadingScreen extends StatelessWidget {
   static const id = 'loading_screen';
   const LoadingScreen({super.key});
 
   @override
-  State<LoadingScreen> createState() => _LoadingScreenState();
-}
-
-class _LoadingScreenState extends State<LoadingScreen> {
-  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: backgroundColor,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SpinKitFoldingCube(
+            SpinKitSquareCircle(
               color: foregroundColor,
-              size: 100,
+              size: 150,
+              duration: const Duration(seconds: 1, milliseconds: 200),
             ),
-            const SizedBox(
-              height: 100,
+            SizedBox(
+              height: (size.height * 0.25) - 50,
             ),
-            Text(
-              "Preparing Quiz",
-              style: TextStyle(
-                fontSize: 25,
-                color: foregroundColor,
+            Padding(
+              padding: EdgeInsets.only(bottom: size.height * 0.25),
+              child: Text(
+                "Preparing Quiz",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: foregroundColor,
+                ),
               ),
             ),
           ],
